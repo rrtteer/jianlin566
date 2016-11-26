4f7046a539beaf172b2bef941221867f4a06a18c
rrtteer <jianlin566@icloud.com>
2016-11-26 14:31:26 +0800

    增删改

--- src/main/java/com/zjl/zjl_crm/dao/SaleChanceDao.java
+++ src/main/java/com/zjl/zjl_crm/dao/SaleChanceDao.java
@@ -2,6 +2,8 @@ package com.zjl.zjl_crm.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.github.miemiedev.mybatis.paginator.domain.PageBounds;
 import com.zjl.zjl_crm.model.SaleChance;
 import com.zjl.zjl_crm.query.SaleChanceQuery;
@@ -15,5 +17,27 @@ public interface SaleChanceDao {
 	 * @return
 	 */
 	public List<SaleChance> selectForPage(SaleChanceQuery query, PageBounds pageBounds);
+
+	/**
+	 * 插入数据
+	 * @param saleChance
+	 * @return
+	 */
+	public int insert(SaleChance saleChance);
+	
+	/**
+	 * 修改
+	 * @param saleChance
+	 * @return
+	 */
+	public int update(SaleChance saleChance);
+	
+	
+	/**
+	 * 批量删除
+	 * @param ids
+	 * @return
+	 */
+	public int deleteBatch(@Param(value="ids") String ids);
 
 }
